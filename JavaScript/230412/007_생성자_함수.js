4036044f9bd8e5651bcba6ebcf1fefd77411174f
jiwon6635 <piok66372@naver.com>
2023-04-13 22:53:21 +0900

    📝 생성자 함수 코드 추가

--- JavaScript/230412/007_생성자_함수.js
+++ JavaScript/230412/007_생성자_함수.js
@@ -22,7 +22,6 @@ function Book(책이름, 책가격, 저자, 출판일) {
     this.출판일 = 출판일
 }
 
-
 let newBook2 = Book("JavaSCript", 100000, "홍길동", "2023.04.12") // undefined? why? return 값이 없어서!
 
 let newBook3 = new Book("JavaSCript", 100000, "홍길동", "2023.04.12")
@@ -32,6 +31,21 @@ let newBook3 = new Book("JavaSCript", 100000, "홍길동", "2023.04.12")
 // new 키워드를 사용했을 때 일어나는 일
 function Book(책이름, 책가격, 저자, 출판일) {
     // this = {}
+    this.책이름 = 책이름
+    this.책가격 = 책가격
+    this.저자 = 저자
+    this.출판일 = 출판일
+    // return this
+}
+
+// this 출력
+let newBook4 = new Book("JavaSCript", 100000, "홍길동", "2023.04.12")
+
+// new 키워드를 사용했을 때 일어나는 일
+function Book(책이름, 책가격, 저자, 출판일) {
+    this.출력 = function () {
+        console.log(this)
+    }
     this.책이름 = 책이름
     this.책가격 = 책가격
     this.저자 = 저자
