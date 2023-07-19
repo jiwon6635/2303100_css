4552d0f13181fe07126bbae932fe4f7bda3c816f
jiwon6635 <piok66372@naver.com>
2023-07-19 09:34:39 +0900

    🐛 shadow generator 배경, box, button 색상 변경 기능 추가

--- Practice/shadow_generator/index.js
+++ Practice/shadow_generator/index.js
@@ -3,12 +3,12 @@ const preview = document.getElementById("preview"),
     ranges = document.querySelectorAll(".settings input"),
     copyButton = document.getElementById("copy-styles");
 
-// Add event listener to each range input
+// input 이벤트 리스너 추가
 ranges.forEach((slider) => {
     slider.addEventListener("input", generateStyles);
 });
 
-// Function to generate and update styles
+// tmxkdlf gkatn
 function generateStyles() {
     const xShadow = document.getElementById("x-shadow").value;
     const yShadow = document.getElementById("y-shadow").value;
@@ -19,19 +19,25 @@ function generateStyles() {
     const shadowInset = document.getElementById("inset-shadow").checked;
     const borderRadius = document.getElementById("border-r").value;
 
-    // Create the box shadow CSS property value
+    // box-shadow 값 생성
     const boxShadow = `${shadowInset ? "inset " : ""} ${xShadow}px ${yShadow}px ${blurRadius}px ${spreadRadius}px ${hexToRgba(shadowColor, shadowOpacity)}`;
 
-    // Update the preview element styles
+    // preview 스타일
     preview.style.boxShadow = boxShadow;
     preview.style.borderRadius = `${borderRadius}px`;
 
-    // Update textarea with generated styles
+    // textarea에 업데이트
     styles.textContent = `box-shadow: ${boxShadow};\nborder-radius: ${borderRadius}px;`;
+    
+    // background, box color, button 색상 통일 (변경) 
+    const backgroundColor = document.getElementById("background-color").value;
+    preview.style.backgroundColor = backgroundColor;
+    document.body.style.backgroundColor = backgroundColor;
+    copyButton.style.backgroundColor = backgroundColor;
 
 }
 
-// Function to convert hex color and opacity to rgba format
+// hex color code
 function hexToRgba(shadowColor, shadowOpacity) {
     const r = parseInt(shadowColor.substr(1, 2), 16);
     const g = parseInt(shadowColor.substr(3, 2), 16);
@@ -40,7 +46,7 @@ function hexToRgba(shadowColor, shadowOpacity) {
     return `rgba(${r}, ${g}, ${b}, ${shadowOpacity})`;
 }
 
-// Function to copy the generated styles
+// 복사 함수
 function copyStyles() {
     styles.select();
     document.execCommand("copy");
@@ -49,5 +55,11 @@ function copyStyles() {
         copyButton.innerText = "Copy Styles";
     }, 500);
 }
-
 generateStyles();
+
+document.body.style.backgroundColor = "#cce1ad";
+copyButton.style.backgroundColor = "##cce1ad";
+preview.style.backgroundColor = "#cce1ad";
+
+
+
