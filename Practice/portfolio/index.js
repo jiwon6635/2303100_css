9ea0320e14e7a211174ba4753021164f60895410
jiwon6635 <piok66372@naver.com>
2023-08-22 18:59:56 +0900

    📝배경 효과 추가 및 디자인 수정

--- Practice/portfolio/index.js
+++ Practice/portfolio/index.js
@@ -2,7 +2,7 @@ particlesJS("background", {
 
     particles: {
         number: {
-            value: 15, // Number of Particles (count)
+            value: 50, // Number of Particles (count)
             density: {
                 enable: true,
                 value_area: 300, // Area where particles will be distributed
@@ -10,27 +10,33 @@ particlesJS("background", {
         },
 
         color: {
-            value: "#ffffff", // Particles color
+            value: ["#DEF5E5", "#BCEAD5", "#C4DFDF", "#8EC3B0"], // Particles color
+            // value: ["#A0C49D", "#C4D7B2", "#E1ECC8", "#F7FFE5"], // Particles color
+            // value: ["#FEF5ED", "#D3E4CD", "#ADC2A9", "#99A799"], // Particles color
+            // value: ["#C4DFDF", "#D2E9E9", "#E3F4F4", "#F8F6F4"], // Particles color
+
+
+
         },
         shape: {
-            type: "triangle", // Shape type
+            type: "circle", // Shape type
         },
         opacity: {
-            value: 0.8, // Base opacity of particles
+            value: 0.3, // Base opacity of particles
             random: true,
             anum: {
                 enable: true,
-                speed: 1,
+                speed: 0.5,
                 opacity_min: 0.1,
                 sync: false,
             },
         },
         size: {
-            value: 5, // Base size of particles
-            random: true,
+            value: 800, // Base size of particles
+            random: false,
             anim: {
                 enable: true,
-                speed: 4,
+                speed: 3,
                 size_min: 0.3,
                 sync: false,
             },
@@ -38,9 +44,9 @@ particlesJS("background", {
 
         // Connecting lines
         line_linked: {
-            enable: true,
+            enable: false,
             distance: 150, // Maximum distance between linked particles
-            color: "#ffffff",
+            color: "#000",
             opacity: 0.4,
             width: 1,
         },
@@ -48,11 +54,11 @@ particlesJS("background", {
         // Particle movement
         move: {
             enable: true,
-            speed: 2,
+            speed: 0.2,
             direction: "none",
-            random: false,
+            random: true,
             straight: false,
-            out_mode: "bounce", // Behavior when particles move out of the canvas
+            out_mode: "out", // Behavior when particles move out of the canvas
             bounce: false,
         },
     },
@@ -61,12 +67,12 @@ particlesJS("background", {
         detect_on: "canvas",
         events: {
             onhover: {
-                enable: true, // Enable hover interactivity
-                mode: "repulse",
+                enable: false, // Enable hover interactivity
+                mode: "grab",
             },
             onclick: {
-                enable: true, // Enable for click
-                mode: "push", // Push particles on click
+                enable: false, // Enable for click
+                mode: "remove", // Push particles on click
             },
             resize: true, // Resize particles animation on window resize
         },
